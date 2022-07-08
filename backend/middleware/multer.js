e19a561c920182df52026f6dc25bbcdd1f763f0f
Jim Lambert <jlambert12013@gmail.com>
2022-07-08 08:11:11 -0500

    Added Inventory to API

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,16 +1,16 @@
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
 
 const root = path.dirname(__dirname)
-const public = path.join(root, 'public')
-const images = path.join(public, 'images')
-const catalog = path.join(images, 'catalog')
+const public = path.join(root, "public")
+const images = path.join(public, "images")
+const catalog = path.join(images, "catalog")
 
 //  Multer Storage
 const storage = multer.diskStorage({
   destination: (req, files, cb) => {
-    const dir = req.body.make.split(' ').join('').toLowerCase()
+    const dir = req.body.make.split(" ").join("").toLowerCase()
     const uploadDir = path.join(catalog, dir)
 
     if (!fs.existsSync(uploadDir)) {
@@ -21,10 +21,10 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const year = req.body.year
-    const model = req.body.model.split(' ').join('').toLowerCase()
+    const model = req.body.model.split(" ").join("").toLowerCase()
     const randomNumber = Math.round(Math.random() * 1e9)
     const extension = path.extname(file.originalname.toLowerCase())
-    const uploadFilename = model + year + randomNumber + extension
+    const uploadFilename = model + "-" + year + randomNumber + extension
 
     // Filename
     cb(null, uploadFilename)
